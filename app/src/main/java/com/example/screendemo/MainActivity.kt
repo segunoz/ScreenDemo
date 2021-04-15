5da18704d2dc0c40176883d931a5647a6cfc782d
segunoz <segunoz1@gmail.com>
2021-04-15 10:16:35 +0100

    intial commit

--- app/src/main/java/com/example/screendemo/MainActivity.kt
+++ app/src/main/java/com/example/screendemo/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.screendemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //hides the action bar
         supportActionBar?.hide()
+
+        var switch: Switch = findViewById(R.id.darkMode)
+
+        switch.setOnClickListener(){
+
+            if (switch.isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+        }
+
+
     }
 }
